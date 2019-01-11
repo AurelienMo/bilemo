3eb7101867693faee47bee8c03625e39030c7e66
Aurelien Morvan <morvan.aurelien@gmail.com>
2019-01-12 00:58:38 +0100

    [Init] Initialize project

---
+++ features/bootstrap/BlackfireContext.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Bilemo
+ *
+ * (c) Aurelien Morvan <morvan.aurelien@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Behat\Behat\Context\Context;
+
+
+/**
+ * Class BlackfireContext
+ */
+class BlackfireContext implements Context
+{
+
+}
