ec18632af7df6958679f43d6d59822eb24883b6a
Aurelien Morvan <morvan.aurelien@gmail.com>
2019-01-13 01:10:00 +0100

    [Phone/List] Implement model to manage phones

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
 ];
---
+++ src/Domain/Model/PhoneOs.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Bilemo
+ *
+ * (c) Aurelien Morvan <morvan.aurelien@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Domain\Model;
+
+use App\Domain\Model\Traits\NameTrait;
+
+/**
+ * Class PhoneOs
+ */
+class PhoneOs extends AbstractModel
+{
+    use NameTrait;
+
+    /** @var string */
+    protected $shortName;
+
+    /**
+     * PhoneOs constructor.
+     *
+     * @param string $name
+     * @param string $shortName
+     *
+     * @throws \Exception
+     */
+    public function __construct(
+        string $name,
+        string $shortName
+    ) {
+        $this->name = $name;
+        $this->shortName = $shortName;
+        parent::__construct();
+    }
+
+    /**
+     * @return string
+     */
+    public function getShortName(): string
+    {
+        return $this->shortName;
+    }
+}
